202548e1d0646a000de0dbb27f342d8279c93e3d
thehacka1 <kalakotig@gmail.com>
2014-03-12 17:22:02 +0000

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -2,17 +2,17 @@
 $(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Release name
-PRODUCT_RELEASE_NAME := GT-I9105P
+PRODUCT_RELEASE_NAME := GT-S7270
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Inherit device configuration
-$(call inherit-product, device/samsung/s2vep/device_s2vep.mk)
+$(call inherit-product, device/samsung/s2vep/device_hawaii.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := s2vep
-PRODUCT_NAME := cm_s2vep
+PRODUCT_DEVICE := hawaii
+PRODUCT_NAME := cm_hawaii
 PRODUCT_BRAND := samsung
-PRODUCT_MODEL := GT-I9105P
+PRODUCT_MODEL := GT-S7270
 PRODUCT_MANUFACTURER := samsung
